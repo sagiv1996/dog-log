b6af201f0a4e60cbffbf7646fdb2ca62e7214601
Sagiv <sagivb13@gmail.com>
2024-12-26 15:38:17 +0200

    add option to login by github

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <button @click="client.auth.signInWithOAuth({ provider: 'github' })">
+      Sign In with Github
+    </button>
+    {{ user }}
   </div>
 </template>
+<script setup lang="ts">
+const client = useSupabaseClient();
+const user = useSupabaseUser();
+const { auth } = useSupabaseClient();
+</script>
