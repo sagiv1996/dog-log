415153b710903bc559819e07454277ffe6b9f3a1
Sagiv <sagivb13@gmail.com>
2024-12-27 08:08:18 +0200

    improve login screen

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
-    <button @click="client.auth.signInWithOAuth({ provider: 'github' })">
-      Sign In with Github
-    </button>
-  </div>
+  <UCard :ui="{ body: { base: 'grid grid-cols-3' } }">
+    <div class="text-center">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, qui.
+      Ratione, corrupti est eum suscipit molestiae quo cum velit doloribus
+      fugiat omnis illo quisquam perspiciatis reiciendis aut ab, laboriosam qui.
+    </div>
+
+    <UDivider label="SignIn" orientation="vertical" />
+
+    <div class="space-y-4 flex flex-col justify-center">
+      <UButton
+        color="black"
+        label="Login with GitHub"
+        icon="i-simple-icons-github"
+        block
+        @click="
+          client.auth.signInWithOAuth({
+            provider: 'github',
+          })
+        "
+      />
+      <UButton
+        color="black"
+        label="Login with Google"
+        icon="i-simple-icons-google"
+        block
+        disabled
+      />
+    </div>
+  </UCard>
 </template>
 <script setup lang="ts">
 const client = useSupabaseClient();
-const user = useSupabaseUser();
-const { auth } = useSupabaseClient();
 </script>
