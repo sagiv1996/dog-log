96069e2a71e6850ebe1e7659096f90ca34a3d5df
Sagiv <sagivb13@gmail.com>
2025-01-01 23:45:46 +0200

    get info about dog with rlc

--- server/api/dog.get.ts
+++ server/api/dog.get.ts
@@ -1,8 +1,11 @@
 import { serverSupabaseClient } from "#supabase/server";
+
 export default defineEventHandler(async (event) => {
   const client = await serverSupabaseClient(event);
-  const { data } = await client
-    .from("dog_access")
-    .select("dog(id, name, birth_date), profile(*)");
-  return data;
+
+  const { data} = await client
+    .from("dog")
+    .select("*, dog_access(*, profile(*))");
+
+return data
 });
