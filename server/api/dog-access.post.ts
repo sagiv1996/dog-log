6faa16eeb5abc6374cc7261176e1d772aa74b3e9
Sagiv <sagivb13@gmail.com>
2025-01-01 15:41:45 +0200

    Create api tom insert dog_access by email and dog_id

---
+++ server/api/dog-access.post.ts
@@ -0,0 +1,32 @@
+import { serverSupabaseClient } from "#supabase/server";
+import { z } from "zod";
+
+const dogAccess = z.object({
+  dog_id: z.number(),
+  email: z.string().email(),
+});
+export default defineEventHandler(async (event) => {
+  try {
+    const client = await serverSupabaseClient(event);
+    const body = await readBody(event);
+    const { dog_id, email } = dogAccess.parse(body);
+
+    const { data: profile, error: errorProfile } = await client
+      .from("profile")
+      .select("*")
+      .eq("email", email)
+      .single();
+    if (errorProfile || !profile) {
+      throw "Error with profile";
+    }
+    const { data, error } = await client
+      .from("dog_access")
+      .insert({ dog_id, user_id: profile.id! });
+    if (error || !data) {
+      throw "Error with dog_access";
+    }
+    return profile;
+  } catch (error) {
+    throw error;
+  }
+});
