ce396d59e9a465206d42e96c6c708c0803c85ffd
Sagiv <sagivb13@gmail.com>
2025-01-26 12:30:19 +0200

    replace code into default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="flex flex-col min-h-screen">
+  <UApp>
     <header class="bg-gray-800 text-white">
       <div class="container mx-auto px-4 py-4">
         <div class="flex items-center justify-between">
           <div class="text-lg font-bold">Dog log</div>
 
-          <UPopover v-if="user">
-            <UAvatar :src="user.user_metadata.avatar_url" alt="Avatar" />
+          <UPopover>
+            <UAvatar :src="user!.user_metadata.avatar_url" alt="Avatar" />
 
-            <template #panel>
+            <template #content>
               <UButton
-                color="black"
                 label="Logout"
                 icon="i-simple-icons-personio"
                 block
@@ -21,21 +20,9 @@
         </div>
       </div>
     </header>
-
-    <main class="container mx-auto">
-      <NuxtPwaManifest />
-      <slot />
-    </main>
-
-    <!-- <footer class="bg-gray-900 text-white">
-      <div class="container mx-auto px-4 py-4 text-center">
-        © 2024 Dog log. All rights reserved.
-      </div>
-    </footer> -->
-    <UNotifications />
-  </div>
+    <NuxtPage />
+  </UApp>
 </template>
-
 <script setup lang="ts">
 const client = useSupabaseClient();
 const user = useSupabaseUser();
