62552a53e985d8e5018b9bbbbe1746f92184488a
Sagiv <sagivb13@gmail.com>
2025-02-06 14:15:27 +0200

    add title for nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      titleTemplate: "%s - Dog-Log",
+    },
+  },
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: [
